147155bf2de0ecb73c47a35d7c5a6a5e5757007b
Burak Karahan <burak.karahan@mail.ru>
2018-12-17 03:21:27 +0300

    Update settings.py
    Static Root

--- project50/settings.py
+++ project50/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = None
 STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
